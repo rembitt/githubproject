d7c024bdc636fb5a867f8bc196074da848fbd3cf
rembitt <daviemmanueldesouza@gmail.com>
2026-05-21 16:42:28 -0300

    primeiro commit

---
+++ projetogitinho.py
@@ -0,0 +1,11 @@
+import os
+
+while True:
+    x = int(input("digite um numero: "))
+    if x == 67:
+        print("67676767676767676767676767676767676767676767676767 voce é fodinha")
+    else:
+        print("nada bom")
+
+    n = input()
+    os.system('clear')
